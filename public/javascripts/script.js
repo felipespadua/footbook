dd96aaeca86f4f3a419a311eec0e66a5c0495bd3
Felipe Padua <felipepadua@Mariannas-MBP.localdomain>
2019-10-08 17:48:30 -0300

    inicio da implementacao de busca por proximidade

--- public/javascripts/script.js
+++ public/javascripts/script.js
@@ -25,6 +25,20 @@ const createNewMatch = () => {
 
 // google.maps.event.addDomListener(window, 'load', initialize);
 
+  if (navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition(function (position) {
+      const user_location = {
+        lat: position.coords.latitude,
+        lng: position.coords.longitude
+      };
+      document.getElementById('locationLat').value =  position.coords.latitude;
+      document.getElementById('locationLng').value =  position.coords.longitude;
+      console.log(user_location)
+    })
+  }
+
+
+
 function initialize() {
   var inputSearch = document.getElementById('autocompleteSearch');
   var autocompleteSearch = new google.maps.places.Autocomplete(inputSearch);
@@ -44,26 +58,3 @@ function initialize() {
       });
 }
 google.maps.event.addDomListener(window, 'load', initialize);
-
-
-function distance(lat1, lon1, lat2, lon2, unit) {
-	if ((lat1 == lat2) && (lon1 == lon2)) {
-		return 0;
-	}
-	else {
-		var radlat1 = Math.PI * lat1/180;
-		var radlat2 = Math.PI * lat2/180;
-		var theta = lon1-lon2;
-		var radtheta = Math.PI * theta/180;
-		var dist = Math.sin(radlat1) * Math.sin(radlat2) + Math.cos(radlat1) * Math.cos(radlat2) * Math.cos(radtheta);
-		if (dist > 1) {
-			dist = 1;
-		}
-		dist = Math.acos(dist);
-		dist = dist * 180/Math.PI;
-		dist = dist * 60 * 1.1515;
-		if (unit=="K") { dist = dist * 1.609344 }
-		if (unit=="N") { dist = dist * 0.8684 }
-		return dist;
-	}
-}
--- routes/index.js
+++ routes/index.js
@@ -69,10 +69,12 @@ router.post("/login", passport.authenticate("local", {
 }));
 
 router.get('/matches', ensureAuthenticated, (req, res, next) => {
+  const { locationLat, locationLng } = req.body;
   const user = req.user;
   Match.find()
     .populate("owner")
     .then( matches => {
+        
         res.render('matches', { matches, user } )
     })
     .catch( err => {
@@ -203,5 +205,25 @@ function ensureAuthenticated(req, res, next) {
     res.redirect('/login')
   }
 }
-
+function distance(lat1, lon1, lat2, lon2, unit) {
+	if ((lat1 == lat2) && (lon1 == lon2)) {
+		return 0;
+	}
+	else {
+		var radlat1 = Math.PI * lat1/180;
+		var radlat2 = Math.PI * lat2/180;
+		var theta = lon1-lon2;
+		var radtheta = Math.PI * theta/180;
+		var dist = Math.sin(radlat1) * Math.sin(radlat2) + Math.cos(radlat1) * Math.cos(radlat2) * Math.cos(radtheta);
+		if (dist > 1) {
+			dist = 1;
+		}
+		dist = Math.acos(dist);
+		dist = dist * 180/Math.PI;
+		dist = dist * 60 * 1.1515;
+		if (unit=="K") { dist = dist * 1.609344 }
+		if (unit=="N") { dist = dist * 0.8684 }
+		return dist;
+	}
+}
 module.exports = router;
